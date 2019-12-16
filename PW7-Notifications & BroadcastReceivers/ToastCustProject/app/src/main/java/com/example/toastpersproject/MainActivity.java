6c5efc97f3c5588985888e0998dd006031d077cd
malika <malimad98@gmail.com>
2019-12-16 11:32:29 +0100

    custom toast activity

---
+++ PW7-Notifications & BroadcastReceivers/ToastCustProject/app/src/main/java/com/example/toastpersproject/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.toastpersproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button toast=findViewById(R.id.toastBtn);
+        toast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = new Toast(getApplicationContext());
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                toast.setDuration(Toast.LENGTH_SHORT);
+                toast.setView(getLayoutInflater().inflate(R.layout.custom_toast,null));
+                toast.show();
+
+            }
+        });
+
+    }
+}
