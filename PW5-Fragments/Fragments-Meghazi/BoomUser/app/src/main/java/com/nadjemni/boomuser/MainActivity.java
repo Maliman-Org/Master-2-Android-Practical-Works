8f1002619d2682b18f675de2fbe4c64279ddac6c
Nadjem Nour El Imane <47980833+NINadjem@users.noreply.github.com>
2020-01-07 18:04:32 +0100

    *

---
+++ PW5-Fragments/Fragments-Meghazi/BoomUser/app/src/main/java/com/nadjemni/boomuser/MainActivity.java
@@ -0,0 +1,25 @@
+package com.nadjemni.boomuser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String ACTION_BOOM = "course.examples.permissionexample.boom.boom_action";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ACTION_BOOM));
+            }
+        });
+    }
+}
---
+++ PW5-Fragments/Fragments-Meghazi/WithActionBar/app/src/main/java/com/nadjemni/withactionbar/MainActivity.java
@@ -0,0 +1,14 @@
+package com.nadjemni.withactionbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
